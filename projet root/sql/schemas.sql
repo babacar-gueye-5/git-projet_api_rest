01c8c44e4d1772a597693e52c918772832706e2e
Babacar Gueye <165727899+babacar-gueye-5@users.noreply.github.com>
2024-07-06 22:01:12 +0000

    modification

--- projet root/sql/schemas.sql
+++ projet root/sql/schemas.sql
@@ -16,6 +16,17 @@ CREATE DATABASE "gestion promts"
 -- creation des tables de la base de donnees 
 
 -- creation table des utilisateurs
+DROP TABLE IF EXISTS Admins;
+CREATE TABLE Admins (
+    idAdmin SERIAL PRIMARY KEY,
+    username_Admin VARCHAR(255) NOT NULL,
+    email_Admin VARCHAR(255) UNIQUE NOT NULL,
+    password_Admin VARCHAR(255) NOT NULL,
+);
+
+
+-- creation table des utilisateurs
+DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
     idUser SERIAL PRIMARY KEY,
     username_User VARCHAR(255) NOT NULL,
@@ -26,28 +37,23 @@ CREATE TABLE Users (
     FOREIGN KEY (idGroup) REFERENCES UserGroups(idGroup) ON DELETE SET NULL
 );
 
--- creation table des utilisateurs
-CREATE TABLE Admin (
-    idAdmin SERIAL PRIMARY KEY,
-    username_Admin VARCHAR(255) NOT NULL,
-    email_Admin VARCHAR(255) UNIQUE NOT NULL,
-    password_Admin VARCHAR(255) NOT NULL,
-);
-
 
 -- creation table des Prompts
+DROP TABLE IF EXISTS Prompts;
 CREATE TABLE Prompts (
-	idPrompts SERIAL PRIMARY KEY,
+	idPrompt SERIAL PRIMARY KEY,
 	titre vARCHAR (255) NOT NULL,
 	description TEXT NOT NULL,
     status VARCHAR(50) DEFAULT 'En attente', -- État initial
+    prixPrompt
 	date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     idUser INTEGER,
     FOREIGN KEY (idUser) REFERENCES Users(idUser) ON DELETE SET NULL
 );
 
--- creation table Groupe 
 
+-- creation table Groupe 
+DROP TABLE IF EXISTS UserGroups;
 CREATE TABLE UserGroups (
     idGroup SERIAL PRIMARY KEY,
     nameGroup VARCHAR(255) NOT NULL,
@@ -57,13 +63,23 @@ CREATE TABLE UserGroups (
     FOREIGN KEY (idAdmin) REFERENCES Admin(idAdmin) ON DELETE SET NULL   
 );
 
--- creation table Vote 
 
+-- creation table Vote 
+DROP TABLE IF EXISTS Vote;
 CREATE TABLE Vote (
 	vote INTEGER,
 	idUser INTEGER,
-	idPrompts INTEGER,
+	idPrompt INTEGER,
 	FOREIGN KEY (idUser) REFERENCES Users(idUser) ON DELETE CASCADE,
-	FOREIGN KEY (idPrompts) REFERENCES Prompts(idPrompts) ON DELETE CASCADE
+	FOREIGN KEY (idPrompt) REFERENCES Prompts(idPrompt) ON DELETE CASCADE
 );
 
+-- creation table Note 
+DROP TABLE IF EXISTS Note;
+CREATE TABLE Note (
+	note INTEGER,
+	idUser INTEGER,
+	idPrompt INTEGER,
+	FOREIGN KEY (idUser) REFERENCES Users(idUser) ON DELETE CASCADE,
+	FOREIGN KEY (idPrompt) REFERENCES Prompts(idPrompt) ON DELETE CASCADE
+);
